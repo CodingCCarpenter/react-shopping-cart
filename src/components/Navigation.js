e574ab4ae29d417407a223dba77c7ceca8c1802b
CodingCCarpenter <christineassists@aol.com>
2019-11-17 20:16:04 -0600

    complete mvp

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
-import CartContext from './contexts/CartContext.js';
+import CartContext from '../contexts/CartContext.js';
 
 
 const Navigation = () => {
